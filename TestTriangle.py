08a1879696533f1968fb061e9886d0c057e093ca
Jay Dwivedi <jayraj1999@gmail.com>
2022-09-22 15:31:34 -0400

    fixed all the bugs

--- TestTriangle.py
+++ TestTriangle.py
@@ -23,8 +23,23 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTriangleA(self):
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testInvalidTriangleA(self):
+        self.assertEqual(classifyTriangle(1, 2, 5), 'NotATriangle', '1,2,5 is not a triangle')
+
+    def testInvalidTriangleB(self):
+        self.assertEqual(classifyTriangle(2, 2, 5), 'NotATriangle', '2,2,5 is not a triangle')
+
+    def testScaleneTriangleA(self):
+        self.assertEqual(classifyTriangle(5, 2, 4), 'Scalene', '5,2,4 is a Right triangle')
+
+    def testEquilateralTriangleC(self):
+        self.assertEqual(classifyTriangle(4, 4, 4), 'Equilateral', '4,4,4 should be equilateral')
+
+    def testInvalidInteger(self):
+        self.assertEqual(classifyTriangle(4, "four", 4), 'InvalidInput', 'a string value cannot be accepted')
 
 if __name__ == '__main__':
     print('Running unit tests')
